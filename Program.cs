54f568ae2d3a48d692a6b3e5892a5471d537e437
Lucas Batista <lucas.aps11@gmail.com>
2022-12-27 21:03:24 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insira o Tempo: ");
+            int t = int.Parse(Console.ReadLine());
+            Start(t);
+        }
+
+        static void Start(int time)
+        {
+
+            int currentTime = 0;
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch Finalizado");
+            Thread.Sleep(2500);
+        }
+    }
+}
